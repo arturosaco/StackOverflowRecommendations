bc568994724e877007d77c19afb2778953475541
Sergio Gómez <sergomezcol@gmail.com>
2013-03-07 17:59:07 +0000

    Retrieving creation date

--- src/download_data.py
+++ src/download_data.py
@@ -1,5 +1,11 @@
 from pandas import Series, DataFrame
-import requests, re, json, pandas as pd, numpy as np, sys, time
+import requests
+import re
+import json
+import pandas as pd
+import numpy as np
+import sys
+import time
 
 # =========
 # = Usage =
@@ -15,17 +21,21 @@ def get_answers_df(url_answers):
   a_to_q_json = requests.get(url_answers).json()
   q_ids = []
   frames = []
-  q_id_text = {}  
+  q_id_text = {}
+  q_id_date = {}  
   
   print a_to_q_json.keys()
   
   if "backoff" in a_to_q_json.keys():
     print "Backoff for" + str(a_to_q_json["backoff"]) + " seconds"
-    time.sleep(a_to_q_json["backoff"])
+    #time.sleep(a_to_q_json["backoff"])
+  
+  #time.sleep(0.1)
 
   for q in a_to_q_json["items"]:
     if "answers" in q.keys():
       q_id_text[q["question_id"]] = q["body"]
+      q_id_date[q["question_id"]] = q["creation_date"]
       q_ids.append(q["question_id"])    
       a_ids = []
       frames_2 = []
@@ -40,6 +50,11 @@ def get_answers_df(url_answers):
   q_id_text_df.index.names = ["question_id"]
   q_id_text_df = q_id_text_df.reset_index("question_id")
 
+  q_id_date_df = DataFrame.from_dict(q_id_date, orient = "index")
+  q_id_date_df.columns = ["creation_date"]
+  q_id_date_df.index.names = ["question_id"]
+  q_id_date_df = q_id_date_df.reset_index("question_id")
+
   answers_out = pd.concat(frames, keys = q_ids)  
   answers_out["owner"] = answers_out["owner"].apply(lambda x : 
     x.values()[0] if(len(x) > 0) else np.nan)
@@ -50,6 +65,8 @@ def get_answers_df(url_answers):
   answers_out = answers_out.reset_index("question_id")
   answers_out = answers_out.reset_index("answer_id")
   answers_out = pd.merge(answers_out, q_id_text_df, on = "question_id")
+  answers_out = pd.merge(answers_out, q_id_date_df, on = "question_id")
+
   return answers_out
 
 def main():
@@ -71,18 +88,22 @@ def main():
   page = "page=1"
   dates = "fromdate=1359676800&todate=1362009600"
   site = "site=stackoverflow"
+  order = "order=asc"
+  sort = "sort=creation"
   #filtro_answers = "filter=!OduD9Oq(n969EztJer8LsVbnB38.3hU.m.L-3u*5IlF"
-  filtro_answers = "filter=!17YvvVMnKqQnZ(4tvXc6y-t7GOxx.voHqEUOcSrH7cbbn7"
+  #filtro_answers = "filter=!17YvvVMnKqQnZ(4tvXc6y-t7GOxx.voHqEUOcSrH7cbbn7"
+  filtro_answers = "filter=!OduD9Ow1h(GaiYDK40EJH_uudJem98vpg3gGIx53ab7"
   answers_list = []
   for k in range(no_pages):
-    print "Retrieving page " + str(k + 1)
-    url_answers = base + dataset + "&".join(["page=" + str(k + 1),
-      pagesize, dates, site, filtro_answers])
-    answers_list.append(get_answers_df(url_answers))
+    if (k%2) == 0:
+    #if (k%2) == 1:
+      print "Retrieving page " + str(k + 1)
+      url_answers = base + dataset + "&".join(["page=" + str(k + 1),
+        pagesize, dates, site, order, sort, filtro_answers])
+      answers_list.append(get_answers_df(url_answers))
 
   answers = pd.concat(answers_list)
   answers = answers.set_index("question_id")
-
   answers.to_csv(output_path, encoding = "UTF-8")
 
 if __name__ == '__main__':
